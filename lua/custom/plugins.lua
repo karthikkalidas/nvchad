733a667b0d6985c787331ead38018a64b64280fc
Karthik Kalidas <karthik.kalidas@gmail.com>
2023-07-16 17:41:30 -0600

    added lazygit

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -98,6 +98,11 @@ local plugins = {
     event = {"CmdlineEnter"},
     ft = {"go", 'gomod'},
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-  }
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    event = "VeryLazy",
+  },
 }
+
 return plugins
